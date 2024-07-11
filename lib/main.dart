6cb4a3c615fecac07b8b45ed8663aa0874b05e56
A3ToF <mohamedandmostafa77@gmail.com>
2024-07-11 19:05:20 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:chatapp/firebase_options.dart';
 import 'package:chatapp/views/login_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChatApp());
 }
 
